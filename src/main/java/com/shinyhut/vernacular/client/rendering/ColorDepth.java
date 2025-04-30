2f446b381781717f3106f5d66005e38630a1361c
Dan Berkowitz <dberkowitz@palantir.com>
2025-04-30 17:47:01 -0400

    Reorder ColorDepth to industry standard and what Initializer is using

--- src/main/java/com/shinyhut/vernacular/client/rendering/ColorDepth.java
+++ src/main/java/com/shinyhut/vernacular/client/rendering/ColorDepth.java
@@ -6,25 +6,39 @@ public enum ColorDepth {
     BPP_8_INDEXED(8, 8, false, 0, 0, 0, 0, 0, 0),
 
     /** 8 bits per pixel true color **/
-    BPP_8_TRUE(8, 8, true, 7, 3, 7, 0, 6, 3),
+    BPP_8_TRUE(8, 8, true, 7, 7, 3, 5, 2, 0),
+//    BPP_8_TRUE(8, 8, true, 7, 7, 3, 0, 3, 6),
 
-    /** 16 bits per pixel true color **/
+    /** 16 bits per pixel true color, RGB565 **/
     BPP_16_TRUE(16, 16, true, 31, 63, 31, 11, 5, 0),
 
-    /** 24 bits per pixel true color **/
-    BPP_24_TRUE(32, 24, true, 255, 255, 255, 8, 24, 16);
+    /** 24 bits per pixel true color, RGB888 **/
+    BPP_24_TRUE(32, 24, true, 255, 255, 255, 16, 8, 0);
 
     private final int bitsPerPixel;
     private final int depth;
     private final boolean trueColor;
     private final int redMax;
-    private final int blueMax;
     private final int greenMax;
+    private final int blueMax;
     private final int redShift;
-    private final int blueShift;
     private final int greenShift;
+    private final int blueShift;
 
-    ColorDepth(int bitsPerPixel, int depth, boolean trueColor, int redMax, int blueMax, int greenMax, int redShift, int blueShift, int greenShift) {
+    /**
+     * Create a ColorDepth object to track RGB elements of the connection
+     *
+     * @param bitsPerPixel Total bits per pixel
+     * @param depth Total bits responsible for color data
+     * @param trueColor Is true color and not indexed color
+     * @param redMax max value for red
+     * @param greenMax max value for green
+     * @param blueMax max value for blue
+     * @param redShift bit shift to get red out of presented data
+     * @param greenShift bit shift to get green out of presented data
+     * @param blueShift bit shift to get blue out of presented data
+     */
+    ColorDepth(int bitsPerPixel, int depth, boolean trueColor, int redMax, int greenMax, int blueMax, int redShift, int greenShift, int blueShift) {
         this.bitsPerPixel = bitsPerPixel;
         this.depth = depth;
         this.trueColor = trueColor;
@@ -52,23 +66,23 @@ public enum ColorDepth {
         return redMax;
     }
 
-    public int getBlueMax() {
-        return blueMax;
-    }
-
     public int getGreenMax() {
         return greenMax;
+    }
+
+    public int getBlueMax() {
+        return blueMax;
     }
 
     public int getRedShift() {
         return redShift;
     }
 
-    public int getBlueShift() {
-        return blueShift;
-    }
-
     public int getGreenShift() {
         return greenShift;
+    }
+
+    public int getBlueShift() {
+        return blueShift;
     }
 }
